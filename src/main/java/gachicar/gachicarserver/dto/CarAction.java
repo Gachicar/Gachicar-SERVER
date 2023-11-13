a32250c98c0c2957eca9652c9461cd24786efeaf
yeonsu <bestdustn@sookmyung.ac.kr>
2023-11-13 15:57:15 +0900

    [FEAT] Add enums - CarAction, CarStatus, HttpStatusCode 생성
    related to #1

---
+++ src/main/java/gachicar/gachicarserver/dto/CarAction.java
@@ -0,0 +1,8 @@
+package gachicar.gachicarserver.dto;
+
+/**
+ * 공유차량에게 내리는 명령
+ */
+public enum CarAction {
+    GO_HOME, WHERE_ARE_YOU, COME_HERE
+}
---
+++ src/main/java/gachicar/gachicarserver/dto/CarStatus.java
@@ -0,0 +1,8 @@
+package gachicar.gachicarserver.dto;
+
+/**
+ * 공유차량의 현재 상태
+ */
+public enum CarStatus {
+    GOOD, BAD_CONNECTION
+}
